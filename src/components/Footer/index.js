dacde9fe2e3213a6ad188ad7f2eb936e32ae6a99
Salimou95 <salimoudiaby95@gmail.com>
2025-12-28 15:42:20 +0100

    initialisation du projet

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <footer>
+            <div className={"footer-container"}>
+                <p>Projet réalisé par Salimou - 2025</p>
+                <p>Les icones Wolverine, Iron-man, Spider-man et Batman sont prise depuis le site iconfinder.com</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+const Header = () => {
+
+    return (
+        <header className={"banner-container"}>
+            <h1><a>Marvel Quiz</a></h1>
+        </header>
+    )
+}
+export default Header;
