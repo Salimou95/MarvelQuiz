3f0d3986db030c60fd9b202b4dcddd550aaf6561
Salimou95 <salimoudiaby95@gmail.com>
2025-12-29 05:41:17 +0100

    Creation du contenue de la page d'accueil

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,36 @@
-const Landing = () => {
-    return (
-        <main className="welcomePage">
+import {useRef, useEffect, useState, Fragment} from "react";
 
+const Landing = () => {
+
+    const [btn, setbtn] = useState(false);
+    const refWolverine = useRef(null);
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            refWolverine.current.classList.add("startingImg");
+            setbtn(true);
+        },1000)
+    }, []);
+
+    const displayBtn = btn &&(
+        <Fragment>
+            <div className="leftBox">
+                <button className="btn-welcome">
+                    Inscription
+                </button>
+            </div>
+            <div className="rightBox">
+                <button className="btn-welcome">
+                    Connexion
+                </button>
+            </div>
+        </Fragment>
+    )
+
+    return (
+        <main ref={refWolverine} className="welcomePage">
+            {displayBtn}
         </main>
     );
 }
